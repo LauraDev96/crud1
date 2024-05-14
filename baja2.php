74c8e6d6be15705afdb537ef47a3664662c78ff7
Laura Molina <girlyheavy96_alina@hotmail.com>
2024-05-14 18:48:09 +0200

    dar de baja los productos

---
+++ baja2.php
@@ -0,0 +1,11 @@
+<?php
+    include "conexion.php";
+
+    mysqli_select_db($conexion,"productosbd");
+
+    $productoborrar = $_GET["id"];
+    $borrar = "DELETE FROM productos WHERE id_producto = '$productoborrar'";
+    mysqli_query($conexion,$borrar);
+    header("Location: baja_ok.php");
+?>
+
